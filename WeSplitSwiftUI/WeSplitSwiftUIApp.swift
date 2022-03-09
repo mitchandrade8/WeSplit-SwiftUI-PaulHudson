e3a532a1ed87927e7d6d153806c1c517bd4b998a
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-03-08 18:15:49 -0700

    Initial Commit

---
+++ WeSplitSwiftUI/WeSplitSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitSwiftUIApp.swift
+//  WeSplitSwiftUI
+//
+//  Created by Mitch Andrade on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
